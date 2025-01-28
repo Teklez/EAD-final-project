fb5e75384cc8a850234c9bc124ba0635ab6dd1db
yonathan <yonatanalebachew7@gmail.com>
2025-01-28 11:38:26 +0300

    m

--- project-management-app/src/main/java/com/anayonzem/project_management_app/controller/ProjectController.java
+++ project-management-app/src/main/java/com/anayonzem/project_management_app/controller/ProjectController.java
@@ -1,15 +1,42 @@
 package com.anayonzem.project_management_app.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 
+import com.anayonzem.project_management_app.model.Project;
+import com.anayonzem.project_management_app.repository.ProjectRepository;
+import com.anayonzem.project_management_app.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class ProjectController {
 
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private ProjectRepository projectRepository;
+
     @GetMapping("/project")
     public String getProject() {
         return "project";
+    }
+
+    @GetMapping("/addProject")
+    public String showAddProjectForm(Model model) {
+        model.addAttribute("users", userRepository.findAll());
+        return "addProject";
+    }
+
+    @PostMapping("/project/add")
+    public String addProject(@ModelAttribute Project project) {
+        projectRepository.save(project);
+        return "redirect:/project";
     }
 
 }
