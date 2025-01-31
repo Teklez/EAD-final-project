2dde4d79e4bed5827bdcc99fcb5c159e8e8dcb59
Teklez <zemenumekiria@gmail.com>
2025-01-31 15:42:11 +0300

    feat():add tasks page

--- project-management-app/src/main/java/com/anayonzem/project_management_app/controller/TaskController.java
+++ project-management-app/src/main/java/com/anayonzem/project_management_app/controller/TaskController.java
@@ -2,16 +2,19 @@ package com.anayonzem.project_management_app.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.ui.Model;
 
 @Controller
 public class TaskController {
 
-    @GetMapping("/tasks")
-    public String getTasks() {
+    @GetMapping("/project/{id}/tasks")
+    public String getTasks(@PathVariable("id") Long projectId, Model model) {
+        model.addAttribute("projectId", projectId);
         return "tasks";
     }
 
-    @GetMapping("/tasks/add")
+    @GetMapping("/project/{id}/tasks/add")
     public String addTask() {
         return "addTask";
     }
